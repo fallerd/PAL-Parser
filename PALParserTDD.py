a545168e60e67ebc75cb7f11fa6c1fc4e92391df
fallerd <fallerd@gmail.com>
2018-02-09 10:27:49 -0700

    working on start/end method

--- PALParserTDD.py
+++ PALParserTDD.py
@@ -11,7 +11,7 @@ class parseFile(object):
 
     code = []
     programs = []
-    startEnd = []
+    #startEnd = []
     start = False
 
     def __init__(self, fileName):
@@ -28,9 +28,29 @@ class parseFile(object):
 
     # scan line
     def parseLine(self, lineNum, line):
-        print(lineNum, line)
+        self.startEnd(lineNum, line)
+        self.code.append([lineNum,line])
+        #print(lineNum, line)
+
+    # find starts and ends
+    def startEnd(self, lineNum, line):
+        if "SRT" in line:
+            if start == False:
+                self.startEnd.append(['start', lineNum])
+                start = True
+            else:
+                print('SRT at line', lineNum, 'does not have matching END')
+        if "END" in line and 'BEQ' not in line and 'BR' not in line and 'BGT' not in line:
+            if start == True:
+                self.startEnd[index].append('end')
+                self.startEnd[index].append(lineNum)
+                start = False
+                index += 1
+            else:
+                print('END at line', lineNum + 1, 'does not have matching SRT')
 
 
+    # remove anything after ';'
     def removeComment(self, line):
         if ';' in line:
             noComment = line.split(';', 1)[0]
@@ -39,32 +59,13 @@ class parseFile(object):
             return line
 
 
-
-    #find matching pairs of SRT END and note lines
-    def findPrograms(self, file):
-        index = 0
-        start = False
-        for lineNum, line in enumerate(file):
-            if "SRT" in line:
-                if start == False:
-                    self.startEnd.append(['start', lineNum])
-                    start = True
-                else:
-                    print('SRT at line', lineNum, 'does not have matching END')
-            if "END" in line and 'BEQ' not in line and 'BR' not in line and 'BGT' not in line:
-                if start == True:
-                    self.startEnd[index].append('end')
-                    self.startEnd[index].append(lineNum)
-                    start = False
-                    index += 1
-                else:
-                    print('END at line', lineNum + 1, 'does not have matching SRT')
-
-
 if '__main__' == __name__:
     fileName = "palprogs"
     fileName +=".pal"
     programs = parseFile(fileName)
+
+    for line in programs.code:
+        print(line[0], line[1])
 
     #for program in programs.list():
         #print(program.startLine)
